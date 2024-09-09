5ce1ef7ca668fcc8c23885ef0a3818121741bd09
Yuri Garcia Ribeiro <81641949+YuriGarciaRibeiro@users.noreply.github.com>
2024-09-09 15:36:50 -0300

    update

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -3,11 +3,25 @@ using Microsoft.OpenApi.Models;
 using WebApi.shared.appDbContext;
 using WebApi.shared.mapping;
 using AspNet.Scalar;
-using WebApi.shared.filters;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenAnyIP(80);
+});
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        builder => builder
+            .AllowAnyOrigin()
+            .AllowAnyHeader()
+            .AllowAnyMethod());
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -15,19 +29,6 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
-    // Configurar para não gerar esquemas para tipos genéricos
-    // var cont = 0;
-    // c.CustomSchemaIds(type =>
-    // {
-    //     if (type.FullName.StartsWith("WebApi"))
-    //     {   
-    //         return type.FullName.Split(".").Last();
-    //     }
-    //
-    //     cont++;
-    //     return $"id{cont}";
-    // });
-    // c.SchemaFilter<SwaggerSchemaFilter>();
 });
 
 builder.Services.AddAutoMapper(typeof(MappingProfile));
@@ -40,7 +41,7 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite($"Data Source={dbPath}"));
 
 var app = builder.Build();
-
+app.UseCors("AllowAll");
 
 app.UseSwagger();
 app.UseSwaggerUI();
